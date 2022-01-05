e5f2ba146c72931787a956902ec3b17f9babd259
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-01-05 10:28:27 +0900

    Add files via upload

---
+++ import_checker.py
@@ -0,0 +1,64 @@
+#-------------------------------------------------------------------------------
+# Name:         Import Checker
+# Purpose:      Importしたライブラリを表示するデスクトップアプリケーション
+#
+# Author:       Shaneron
+#
+# Created:      2021/01/04
+# Copyright:    (c) Shaneron 2021
+#-------------------------------------------------------------------------------
+
+def main():
+    try:
+        import sys
+        import time
+
+        print("〇調査したいファイルをドラッグ&ドロップしてEnterを押してください。")
+        read_file = input()
+
+        import_contents, from_contents = check_import(read_file)
+
+        print('・importしたライブラリはこちら↓\n')
+        for content in import_contents:
+            time.sleep(0.1)
+            print(f"{content}")
+
+        print('\n・importしたライブラリはこちら(fromスタート)↓\n')
+        for content in from_contents:
+            time.sleep(0.1)
+            print(f"{content}")
+
+
+        input("\nEnterで終了します。")
+
+    except ModuleNotFoundError as NO_MODULE_ERROR:
+        print(f"ModuleNotFoundError: {NO_MODULE_ERROR} at main")
+    except FileNotFoundError as NOT_FOUND_ERROR:
+        print(f"ModuleNotFoundError: {NOT_FOUND_ERROR} at main")
+
+def check_import(path):
+    import_data = []
+    from_data = []
+
+    with open(path, encoding="utf-8") as file_data:
+        lines = file_data.readlines()
+
+    replace_line = [linedata.replace(' ', '') for linedata in lines]
+    replace_and_strip_line = [replacedata.replace('\n', '') for replacedata in replace_line]
+
+    for pyline in replace_and_strip_line:
+        if pyline.startswith("import", 0):
+            del_import = pyline.replace('import', '')
+            import_data.append(del_import)
+        elif pyline.startswith("from", 0):
+            del_import = pyline.replace('from', 'from ')
+            del_from = del_import.replace('import', ' import ')
+            from_data.append(del_from)
+
+    import_data = list(set(import_data))
+    from_data = list(set(from_data))
+
+    return import_data, from_data
+
+if __name__ == '__main__':
+    main()
---
+++ import_test.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:         import test
+# Purpose:      テストプログラム
+#
+# Author:       Shaneron
+#
+# Created:      2022/01/05
+# Copyright:   (c) Shaneron 2022
+#-------------------------------------------------------------------------------
+
+def main():
+    import random
+    import numpy
+    import os
+    from math import pi
+
+if __name__ == '__main__':
+    main()
